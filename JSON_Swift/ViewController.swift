21c9aba4e0db5cfabe352617c2567d1244feb3f1
Josh R <seventytwoapp@gmail.com>
2020-11-21 16:37:57 -0600

    Initial Commit

---
+++ JSON_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON_Swift
+//
+//  Created by Josh R on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
